f3053ec5b919859637b20d6f6c8fc55c44d69df2
Louise <llouise@bu.edu>
2025-09-14 12:15:29 -0400

    example 2

--- formdata/urls.py
+++ formdata/urls.py
@@ -7,6 +7,8 @@ from . import views  # from . = local directory
 # URL patterns specific to the hw app:
 # variable name django looks for
 urlpatterns = [
+    path(r"", views.show_form, name="show_form"),
+    path(r"submit", views.submit, name="submit"),
     # path(r"", views.home, name="home"),  # connects empty string to views.home
     # path(r"", views.home_page, name="home_page"),
     # path(r"about", views.about, name="about_page"),
--- formdata/views.py
+++ formdata/views.py
@@ -1,3 +1,36 @@
+# File: formdata/templates/formdata/views.py
+# Name: Louise
+# Description:
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def show_form(request):
+    """Show form to the user"""
+
+    # Variable to hold template - delegate work to display template
+    template_name = "formdata/form.html"
+    return render(request, template_name)
+
+
+def submit(request):
+    """Process form submission, generate result"""
+
+    template_name = "formdata/confirmation.html"
+    print(request.POST)
+
+    # check it POST data was sent with HTTP POST msg:
+    if request.POST:
+        # extract form fields into variables:
+        name = request.POST["name"]
+        favorite_color = request.POST["favorite_color"]
+
+        # Create dictionary - context variables for use in template
+        context = {
+            "name": name,
+            "favorite_color": favorite_color,
+        }
+
+    # delegate response to template, provide context variables
+    return render(request, template_name=template_name, context=context)
