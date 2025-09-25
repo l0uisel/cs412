91d10c3c1e0fbad4c5b2dfc6fd38c65bbad9e235
Louise <llouise@bu.edu>
2025-09-25 11:13:32 -0400

    tweak variable names

--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,4 +1,4 @@
-# File: quotes/views.py
+# File: mini_insta/views.py
 # Author: Louise Lee, llouise@bu.edu, 09/25/2025
 # Description: Defines view function, handles rendering of the different pages by using
 # context data
@@ -9,16 +9,15 @@ from django.views.generic import (
     DetailView,
 )  # look at all instances of model (detail = single instance)
 from .models import Profile
-import random
 
 
 # Create your views here.
 class ProfileListView(ListView):
-    """Define a viw class to show all profiles."""
+    """Define a view class to show all profiles."""
 
     # model name and template you want to use
     model = Profile
-    template_name = "profile/show_all_profiles.html"
+    template_name = "mini_insta/show_all_profiles.html"
     # variable within html page, contain many profile instances
     context_object_name = "profiles"
 
@@ -27,20 +26,5 @@ class ProfileDetailView(DetailView):
     """Display a single profile"""
 
     model = Profile
-    template_name = "profile/show_profile.html"
+    template_name = "mini_insta/show_profile.html"
     context_object_name = "profile"
-
-
-# class RandomArticleView(DetailView):
-#     """Display single article selected at random"""
-
-#     model = Article
-#     template_name = "blog/article.html"
-#     context_object_name = "article"
-
-#     # methods
-#     def get_object(self):
-#         """Return one instance of the Article object selected at random"""
-#         all_articles = Article.objects.all()
-#         article = random.choice(all_articles)
-#         return article
