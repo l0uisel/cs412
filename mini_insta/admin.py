a6db0d989887a6ed3273b155a3bc77ce50725fc1
Louise <llouise@bu.edu>
2025-10-16 14:50:30 -0400

    add likes to admin

--- mini_insta/admin.py
+++ mini_insta/admin.py
@@ -7,10 +7,11 @@ from django.contrib import admin
 # Register your models here.
 
 # give access to django admin to see profile model
-from .models import Profile, Photo, Post, Follow, Comment
+from .models import Profile, Photo, Post, Follow, Comment, Like
 
 admin.site.register(Profile)
 admin.site.register(Photo)
 admin.site.register(Post)
 admin.site.register(Follow)
 admin.site.register(Comment)
+admin.site.register(Like)
